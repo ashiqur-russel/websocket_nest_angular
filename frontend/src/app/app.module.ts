9f7cbd16d5e3cc7ac795a61f746eff8224172859
Ashiqur <ashiqur.rahman@bidi.one>
2024-03-12 14:50:06 +0100

    Remove node_modules from tracking

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    MessageComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
