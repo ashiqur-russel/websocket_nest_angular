589794f6b4d9919a5fad8d28b9ba441523f0ff80
Ashiqur <ashiqur.rahman@bidi.one>
2024-03-25 23:45:00 +0100

    Add authguard implementation

--- frontend/src/app/shared/authguard.guard.ts
+++ frontend/src/app/shared/authguard.guard.ts
@@ -1,5 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../service/shared/auth.service';
 
-export const authguardGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+}
