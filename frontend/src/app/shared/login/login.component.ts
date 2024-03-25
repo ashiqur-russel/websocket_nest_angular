4c68953f01292ea4b393ccaf9d1163751d559b04
Ashiqur <ashiqur.rahman@bidi.one>
2024-03-25 22:13:19 +0100

    Add angular router and login component

--- frontend/src/app/shared/login/login.component.ts
+++ frontend/src/app/shared/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin(){
+    console.log(this.loginForm.value);
+  }
 
 }
