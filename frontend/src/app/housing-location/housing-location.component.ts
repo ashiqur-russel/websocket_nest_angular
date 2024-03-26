96406d91c27e83a69c58e934e594e69cb273741d
Ashiqur <ashiqur.rahman@bidi.one>
2024-03-26 01:07:04 +0100

    Add mock data for listing house

--- frontend/src/app/housing-location/housing-location.component.ts
+++ frontend/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../interface/housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.scss']
 })
 export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
 
 }
